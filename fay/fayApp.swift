d538b1afcebbafddbd4df0c88fb8242f0bb024b0
Cassie Wallace <cassbwallace@gmail.com>
2026-03-18 09:35:23 -0600

    Initial Xcode project

---
+++ fay/fayApp.swift
@@ -0,0 +1,17 @@
+//
+//  fayApp.swift
+//  fay
+//
+//  Created by Cassie Wallace on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct fayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fayTests/fayTests.swift
@@ -0,0 +1,16 @@
+//
+//  fayTests.swift
+//  fayTests
+//
+//  Created by Cassie Wallace on 3/18/26.
+//
+
+import Testing
+
+struct fayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

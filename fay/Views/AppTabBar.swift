896198628bddd01a99e03077523e1c786760be20
Cassie Wallace <cassbwallace@gmail.com>
2026-03-18 16:00:06 -0600

    Use filled calendar icon when appointments tab is selected

--- fay/Views/AppTabBar.swift
+++ fay/Views/AppTabBar.swift
@@ -18,28 +18,33 @@ struct AppTabBar: View {
     // MARK: - Body
 
     let token: String
+    @State private var selectedTab = 0
 
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             AppointmentsList(token: token)
                 .tabItem {
-                    Label(Copy.Tabs.appointments, image: "icon-calendar")
+                    Label(Copy.Tabs.appointments, image: selectedTab == 0 ? "icon-calendar-filled" : "icon-calendar")
                 }
+                .tag(0)
 
             ChatScreen()
                 .tabItem {
                     Label(Copy.Tabs.chat, image: "icon-chats")
                 }
+                .tag(1)
 
             JournalScreen()
                 .tabItem {
                     Label(Copy.Tabs.journal, image: "icon-notebook")
                 }
+                .tag(2)
 
             ProfileScreen()
                 .tabItem {
                     Label(Copy.Tabs.profile, image: "icon-user")
                 }
+                .tag(3)
         }
         .tint(.accentFill.primary)
     }
